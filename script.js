30970958777834e1a4296dfed089d3e0e3d072af
namneau8574 <namneau8574@gmail.com>
2026-06-03 16:37:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -388,3 +388,18 @@ scale(1)
 });
 
 });
+window.addEventListener("load",()=>{
+
+setTimeout(()=>{
+
+document.getElementById("loader").style.opacity="0";
+
+setTimeout(()=>{
+
+document.getElementById("loader").style.display="none";
+
+},500);
+
+},3000);
+
+});
